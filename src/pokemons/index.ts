dceb75b97d648566a07eb26ffffb19e78a6e6da8
pool.deza16@gmail.com <dmillonespoolan@uss.edu.pe>
2024-05-05 14:18:03 -0500

    Primer commit

---
+++ src/pokemons/index.ts
@@ -0,0 +1,5 @@
+export { PokemonGrid } from './interfaces/components/PokemonGrid';
+
+export type { PokemonsResponse } from "./interfaces/pokemons-response";
+export type { SimplePokemon } from "./interfaces/simple-pokemon";
+export type { Pokemon } from "./interfaces/pokemon";
